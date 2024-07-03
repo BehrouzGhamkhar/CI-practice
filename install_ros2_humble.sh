e976e405ccfc94a5580648456d196d48a0e6a377
Behrouz <behroozghamkhar@gmail.com>
2024-07-03 15:55:08 +0200

    update ci.yml

--- install_ros2_humble.sh
+++ install_ros2_humble.sh
@@ -9,8 +9,8 @@ export LANG=en_US.UTF-8
 
 # Setup sources
 sudo apt update && sudo apt install -y curl gnupg2 lsb-release
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | gpg --dearmor -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list > /dev/null
 
 # Install ROS 2 Humble
 sudo apt update
